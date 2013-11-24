7ee40d563553366eb2dbd626056841eca0d6cb63
Colby Reineke <YoshiTheCorgi@Colbys-MacBook-Pro.local>
2013-11-24 15:22:21 -0600

    Initial Commit

---
+++ BigfootReports/btcAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  btcAppDelegate.h
+//  BigfootReports
+//
+//  Created by Colby Reineke on 11/24/13.
+//  Copyright (c) 2013 Colby Reineke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface btcAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ BigfootReports/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BigfootReports
+//
+//  Created by Colby Reineke on 11/24/13.
+//  Copyright (c) 2013 Colby Reineke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "btcAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([btcAppDelegate class]));
+    }
+}
